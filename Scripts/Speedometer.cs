c601f69eaca9bf976357cb9cd13d719425cda9dc
Glenn Golden <ggolden22@max.com>
2023-07-04 13:09:25 -0400

    Speedometer: added UI displays for position and velocity

--- Scripts/Speedometer.cs
+++ Scripts/Speedometer.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Speedometer : MonoBehaviour
 {
-    private float interval = 0.0f;
+    public Text velocityText;
+    public Text positionText;
     public float reportingEvery = 10f;
+
+    private float interval = 0.0f;
     private Rigidbody physics;
 
     // Start is called before the first frame update
@@ -21,6 +25,14 @@ public class Speedometer : MonoBehaviour
         if (interval > reportingEvery) {
             interval -= reportingEvery;
             Debug.Log($"@ z={gameObject.transform.position.z} velocity={physics.velocity}");
+
+            if (velocityText != null) {
+                velocityText.text = $"Velocity: {physics.velocity}";
+            }
+
+            if (positionText != null) {
+                positionText.text = $"Position: {gameObject.transform.position.z}";
+            }
         }
     }
 }
